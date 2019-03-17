b1a6be3dff7c53695fd2221313de4ff3055bd01d
Alexandru Oancea <alx.oancea@gmail.com>
2019-03-17 08:41:32 +0200

    week-03: add duckbill controller

---
+++ week-03/WebApplication1/WebApplication1/Controllers/DuckbillsController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DuckbillsController : ControllerBase
+    {
+        private static List<Duckbill> duckbills = new List<Duckbill>()
+        {
+            new Duckbill() { Id = Guid.NewGuid(), Name = "Perry" },
+            new Duckbill() { Id = Guid.NewGuid(), Name = "Stanley" },
+        };
+
+        // GET: api/Duckbills
+        [HttpGet]
+        public Duckbill[] GetDuckbill()
+        {
+            return duckbills.ToArray();
+        }
+
+        // GET: api/Duckbills/5
+        [HttpGet("{id}")]
+        public Duckbill GetDuckbill(Guid id)
+        {
+            Duckbill duckbill = duckbills.FirstOrDefault(db => db.Id == id);
+
+            return duckbill;
+        }
+
+        // PUT: api/Duckbills
+        [HttpPut]
+        public IActionResult PutDuckbill([FromBody] Duckbill duckbill)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Duckbill dbDuckbill = duckbills.FirstOrDefault(db => db.Id == duckbill.Id);
+            dbDuckbill.Name = duckbill.Name;
+
+            return Ok();
+        }
+
+        // POST: api/Duckbills
+        [HttpPost]
+        public IActionResult PostDuckbill([FromBody] Duckbill duckbill)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            duckbills.Add(duckbill);
+
+            return Ok();
+        }
+
+        // DELETE: api/Duckbills/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteDuckbill([FromRoute] Guid id)
+        {
+            Duckbill duckbill = duckbills.FirstOrDefault(db => db.Id == id);
+
+            duckbills.Remove(duckbill);
+
+            return Ok();
+        }
+    }
+}
--- week-03/WebApplication1/WebApplication1/Startup.cs
+++ week-03/WebApplication1/WebApplication1/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
 
 namespace WebApplication1
 {
@@ -25,6 +27,9 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebApplication1Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebApplication1Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
