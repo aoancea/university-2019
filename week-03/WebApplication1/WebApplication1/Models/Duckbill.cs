86b2a489b21761fb71c2f1edccf8cd1d4f758a46
Alexandru Oancea <alx.oancea@gmail.com>
2019-03-17 08:53:05 +0200

    week-03: add model validations using data annotations

--- week-03/WebApplication1/WebApplication1/Models/Duckbill.cs
+++ week-03/WebApplication1/WebApplication1/Models/Duckbill.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
 {
@@ -6,6 +7,14 @@ namespace WebApplication1.Models
     {
         public Guid Id { get; set; }
 
+        [MaxLength(10, ErrorMessage = "Name is too long. Max length is 10!")]
+        [Required(ErrorMessage = "Name cannot be empty! Please provide a value!")]
         public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, RegularExpression("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")]
+        public string EmailByRegex { get; set; }
     }
 }
