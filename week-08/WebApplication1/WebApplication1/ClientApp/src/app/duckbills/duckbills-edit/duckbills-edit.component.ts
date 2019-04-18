9b2d242e0dc87562c83574335329638526b792ba
aoancea <alx.oancea@gmail.com>
2019-04-18 23:31:56 +0300

    week-08: import changes from week-07

--- week-08/WebApplication1/WebApplication1/ClientApp/src/app/duckbills/duckbills-edit/duckbills-edit.component.ts
+++ week-08/WebApplication1/WebApplication1/ClientApp/src/app/duckbills/duckbills-edit/duckbills-edit.component.ts
@@ -1,102 +1,90 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, AbstractControl, FormArray, FormBuilder } from '@angular/forms';
-import { map } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
-import { Duckbill } from './../duckbills.models';
-import { Router, ActivatedRoute } from '@angular/router';
 import { DuckbillsService } from '../duckbills.service';
-
+import { Duckbill } from './../duckbills.models';
 
 @Component({
-  selector: 'app-duckbills-edit',
-  templateUrl: './duckbills-edit.component.html',
-  styleUrls: ['./duckbills-edit.component.css']
+    selector: 'app-duckbills-edit',
+    templateUrl: './duckbills-edit.component.html',
+    styleUrls: ['./duckbills-edit.component.css']
 })
 export class DuckbillsEditComponent implements OnInit {
 
-  private routerLink: string = '../list';
+    private routerLink: string = '../list';
 
-  public duckbill: Duckbill;
+    public duckbill: Duckbill;
 
-  private duckbillID: string;
+    private duckbillID: string;
 
-  private isEdit: boolean = false;
+    private isEdit: boolean = false;
 
-  public formGroup: FormGroup;
+    public formGroup: FormGroup;
 
-  private errorMessage: string;
+    private errorMessage: string;
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-    private duckbillsService: DuckbillsService,
-    private formBuilder: FormBuilder) { }
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private duckbillsService: DuckbillsService,
+        private formBuilder: FormBuilder) { }
 
-  ngOnInit() {
+    ngOnInit() {
 
-    this.duckbillID = this.route.snapshot.params['id'];
+        this.duckbillID = this.route.snapshot.params['id'];
 
-    if (this.duckbillID)
-      this.routerLink = '../../list';
+        if (this.duckbillID) {
+            this.routerLink = '../../list';
+            this.isEdit = true;
+        }
 
-    this.duckbillsService.loadDuckbill(this.duckbillID).subscribe(res => {
-      this.duckbill = res;
-      this.initForm(this.duckbill ? this.duckbill : <Duckbill>{});
-      this.isEdit = true;
-    });
+        this.duckbillsService.loadDuckbill(this.duckbillID).subscribe(res => {
+            this.duckbill = res;
+            this.initForm(this.duckbill ? this.duckbill : <Duckbill>{});
+        });
 
-  }
+    }
 
-  //save() {
-  //  this.clearErrorMessages();
+    save() {
+        Object.keys(this.formGroup.controls).forEach(control => {
+            this.formGroup.get(control).markAsTouched();
+        });
 
-  //  Object.keys(this.formGroup.controls).forEach(control => {
-  //    this.formGroup.get(control).markAsTouched();
-  //  });
+        if (this.formGroup.valid) {
+            let duckbill = this.formGroup.value as Duckbill;
 
-  //  if (this.formGroup.valid) {
-  //    let periodicElement = this.formGroup.value as PeriodicElement;
+            if (this.isEdit) {
+                duckbill.id = this.duckbillID;
+            }
 
-  //    if (this.isEdit) {
-  //      periodicElement.id = this.periodicElementID;
-  //    }
+            this.duckbillsService.saveDuckbill(duckbill).subscribe(res => {
+                this.router.navigate(['/duckbills']);
+            });
+        }
+    }
 
-  //    this.periodicElementService.savePeriodicElement(periodicElement).subscribe((validationErrors: ValidationError[]) => {
-  //      if (validationErrors.length != 0) {
-  //        this.displayErrorMessages(validationErrors)
-  //      } else {
-  //        this.router.navigate(['/periodic-elements']);
-  //      }
-  //    });
-  //  }
-  //}
 
-  //displayErrorMessages(validationErrors: ValidationError[]) {
-  //  this.errorMessage = validationErrors.map(x => x.message).join(' | ');
-  //}
 
-  //clearErrorMessages() {
-  //  this.errorMessage = null;
-  //}
 
-  initForm(duckbill: Duckbill) {
-    this.formGroup = this.formBuilder.group({
-      name: [duckbill.name, [Validators.required, Validators.max(118)] ],
-    });
-  }
+    initForm(duckbill: Duckbill) {
+        this.formGroup = this.formBuilder.group({
+            name: [duckbill.name, [Validators.required, Validators.maxLength(15)]],
+        });
+    }
 
-  //createIsotopeFormGroup(isotope: Isotope) {
-  //  return this.formBuilder.group({
-  //    name: isotope.name
-  //  });
-  //}
+    //createIsotopeFormGroup(isotope: Isotope) {
+    //  return this.formBuilder.group({
+    //    name: isotope.name
+    //  });
+    //}
 
-  
 
-  //validatePositionTaken(control: AbstractControl) {
-  //  return this.periodicElementService.detailPeriodicElementHeaderByPosition(control.value).pipe(
-  //    map(periodicElement => periodicElement ? null : { positionTaken: true })
-  //  );
-  //}
+
+    //validatePositionTaken(control: AbstractControl) {
+    //  return this.periodicElementService.detailPeriodicElementHeaderByPosition(control.value).pipe(
+    //    map(periodicElement => periodicElement ? null : { positionTaken: true })
+    //  );
+    //}
 
 }
