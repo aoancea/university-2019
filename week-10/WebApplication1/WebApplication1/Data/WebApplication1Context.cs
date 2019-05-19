31db217e71e999d17d51c415a07f6d27c16643ee
Alexandru Oancea <alx.oancea@gmail.com>
2019-05-19 11:04:32 +0300

    week-10: derive WebApplication1Context from IdentityDbContext such that Identity tables will be added. Add a new migration and update the database.

--- week-10/WebApplication1/WebApplication1/Data/WebApplication1Context.cs
+++ week-10/WebApplication1/WebApplication1/Data/WebApplication1Context.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Data
 {
-    public class WebApplication1Context : DbContext
+    public class WebApplication1Context : IdentityDbContext
     {
-        public WebApplication1Context (DbContextOptions<WebApplication1Context> options)
-            : base(options)
+        public WebApplication1Context(DbContextOptions<WebApplication1Context> options)
+             : base(options)
         {
         }
 
