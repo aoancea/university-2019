e431ef4278b4b9645cfc7bfe55b9487d3b5381f1
Alexandru Oancea <alx.oancea@gmail.com>
2019-05-19 11:48:31 +0300

    week-10: add login flow. token is being saved in browser local storage - you can check it by accessing dev tools -> application -> local storage(in Chrome)

--- week-10/WebApplication1/WebApplication1/ClientApp/src/app/login/login.component.ts
+++ week-10/WebApplication1/WebApplication1/ClientApp/src/app/login/login.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { LoginModel, Token } from '../shared/security.models';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    public loginModel: LoginModel = <LoginModel>{};
 
-  ngOnInit() {
-  }
+    constructor(
+        private httpClient: HttpClient,
+        private router: Router,
+        @Inject('BASE_URL') private baseUrl: string) { }
 
+    ngOnInit() {
+    }
+
+    loginUser() {
+        this.httpClient.post<Token>(this.baseUrl + `api/Account/Login`, this.loginModel).subscribe(token => {
+
+            localStorage.setItem("token", JSON.stringify(token));
+
+            this.router.navigate(['/duckbills']);
+        });
+    }
 }
