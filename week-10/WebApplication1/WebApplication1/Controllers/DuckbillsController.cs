83715832b9c3013e982a66cb04a5f90448f109df
Alexandru Oancea <alx.oancea@gmail.com>
2019-05-19 12:39:07 +0300

    week-10: secure Duckbills controller. Only authenticated users can access this data. If users are not authenticated, they will be redirected to '/login' page

--- week-10/WebApplication1/WebApplication1/Controllers/DuckbillsController.cs
+++ week-10/WebApplication1/WebApplication1/Controllers/DuckbillsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +8,7 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]/[action]")]
     public class DuckbillsController : Controller
     {
