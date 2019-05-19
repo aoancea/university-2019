c497bb0edb7b71891eb45d0db9cde915f4af0a40
Alexandru Oancea <alx.oancea@gmail.com>
2019-05-19 11:44:16 +0300

    week-10: add register flow. token is being saved in browser local storage - you can check it by accessing dev tools -> application -> local storage(in Chrome)

--- week-10/WebApplication1/WebApplication1/ClientApp/src/app/register/register.component.ts
+++ week-10/WebApplication1/WebApplication1/ClientApp/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { RegisterModel, Token } from '../shared/security.models';
 
 @Component({
     selector: 'app-register',
@@ -7,8 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-    constructor() { }
+    public registerModel: RegisterModel = <RegisterModel>{};
+
+    constructor(
+        private httpClient: HttpClient,
+        private router: Router,
+        @Inject('BASE_URL') private baseUrl: string) { }
 
     ngOnInit() {
+    }
+
+    registerUser() {
+        this.httpClient.post<Token>(this.baseUrl + `api/Account/Register`, this.registerModel).subscribe(token => {
+
+            localStorage.setItem("token", JSON.stringify(token));
+
+            this.router.navigate(['/duckbills']);
+        });
     }
 }
